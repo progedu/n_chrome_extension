a4adb95560cfde8d757e3096aa15126b4423ed29
DDRAGON <davide.0829@gmail.com>
2018-06-01 14:56:35 +0900

    最新バージョンにアプデート

---
+++ script.js
@@ -0,0 +1,6 @@
+$('.modal-window-inner').css('width', '100%').css('height', '100%').css('left', '0px');
+$('.modal-window-content').css('width', '100%').css('height', '100%');
+$('#modal-inner-iframe').css('width', '100%').css('height', '100%');
+$('#modal-inner-iframe').attr('src', $('#modal-inner-iframe').attr('src'));
+$('.toggle-sidebar-button').css('display', 'none');
+$('.modal-close').css('display', 'none');
